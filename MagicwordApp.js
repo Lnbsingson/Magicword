00031379d1e18951e1e6f04a9982104ed63ad9bc
Tantan <lowellnathanielsingson@gmail.com>
2021-02-25 17:49:36 +0800

    Construction of Magic Words

--- MagicwordApp.js
+++ MagicwordApp.js
@@ -1,2 +1,6 @@
-console.log('hello');
-console.log('hello')
+const MagicWord = {
+    SMate1: ['Hello','Good Morning','Good Afternoon','Good Evening'],
+    SMate2: ['Youre Doing Great!','Youre Unique','Youre one of a kind','Never Give up']
+}
+
+console.log(MagicWord.SMate2[3])
