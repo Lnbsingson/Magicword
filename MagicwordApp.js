b1fe885b353dab02dcb14bed1ebbc208f48b1ab3
Tantan <lowellnathanielsingson@gmail.com>
2021-02-26 09:26:50 +0800

    Initialie word process

--- MagicwordApp.js
+++ MagicwordApp.js
@@ -2,9 +2,14 @@ const MagicWord = {
     SMate1: ['Hello','Good Morning','Good Afternoon','Good Evening'],
     SMate2: ['Youre Doing Great!','Youre Unique','Youre one of a kind','Never Give up'],
     SMate3: ['Youre Doing Great!','Youre Unique','Youre one of a kind','Never Give up'],
-    Process() { 
-        return 'hello'
+    GetRandomNums (){
+        var x =[]
+        for (let i=0; i<4; i++){
+         
+        x[i]=Math.floor(Math.random()*4)
+        }
+        return x
     }
 }
 
-console.log(MagicWord.Process())
+console.log(MagicWord.GetRandomNums())
