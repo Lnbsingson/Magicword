2ac7c6dabeb336b5c1630138591b7c2c094248bd
Tantan <lowellnathanielsingson@gmail.com>
2021-02-26 08:57:04 +0800

    Function initialized

--- MagicwordApp.js
+++ MagicwordApp.js
@@ -1,7 +1,10 @@
 const MagicWord = {
     SMate1: ['Hello','Good Morning','Good Afternoon','Good Evening'],
     SMate2: ['Youre Doing Great!','Youre Unique','Youre one of a kind','Never Give up'],
-    SMate3: ['Youre Doing Great!','Youre Unique','Youre one of a kind','Never Give up']
+    SMate3: ['Youre Doing Great!','Youre Unique','Youre one of a kind','Never Give up'],
+    Process() { 
+        console.log('hello')
+    }
 }
 
-console.log(MagicWord.SMate2[3])
+console.log(MagicWord.Process())
